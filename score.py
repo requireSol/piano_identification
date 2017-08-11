402e531bb2099155feb3e9b0263b64c9adfc7fcc
eh1998 <>
2017-08-11 21:11:34 +0200

    Refactoring Part I

---
+++ score.py
@@ -0,0 +1,36 @@
+import helper
+
+
+class Score:
+
+    def __init__(self, key='C', composer='unknown', title='noname'):
+        self.composer = composer
+        self.title = title
+        self.parts = []
+        self.key = key
+
+    def add(self, part_objects):
+        if isinstance(part_objects, list):
+            for single_object in part_objects:
+                if helper.is_valid_part_object(single_object):
+                    self.parts.append(single_object)
+
+        elif helper.is_valid_part_object(part_objects):
+            self.parts.append(part_objects)
+
+    def convert_to_abc(self):
+        header = 'L:1/16\nK:' + self.key + '\nM:4/4\nC:' + self.composer + '\nT:' + self.title + '\n'
+        body = ''
+        score_instruction = '%%score {'
+        for part in self.parts:
+            temp = part.convert_to_abc()
+            score_instruction += temp[1]
+            for voice in temp[0]:
+                body += voice + '\n'
+
+        score_instruction = score_instruction[:-1] + ' }'
+        header += score_instruction + '\n'
+
+        return header + body
+
+
